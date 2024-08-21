16b25980bc2b6cc1f39cfe5bd135121cc9d27829
Sean <sean.michael.otoole@gmail.com>
2024-08-21 19:58:31 +0000

    sean-otoole
    No longer using gene symbols list, switching to ensembl id then converting to gene symbols with mygene. The reason is that the original dictionary of gene signature lists had numerous entries where the gene signatures were composed of identical genes, however, all lists should have been unique.

--- brca_scripts/getRSigs.py
+++ brca_scripts/getRSigs.py
@@ -3,7 +3,7 @@ from rpy2.robjects import pandas2ri, conversion
 import rpy2.robjects as robjects
 import pandas as pd
 import numpy as np
-
+import mygene
 
 def getRSigs():
 
@@ -11,7 +11,7 @@ def getRSigs():
     pandas2ri.activate()
     
     # Path to the .rds file
-    rds_path = os.path.join(os.getcwd(), 'gene_signatures', 'all_signature_list_symbol.Rds')
+    rds_path = os.path.join(os.getcwd(), 'gene_signatures', 'all_signature_list_ensembl.Rds')
     
     # Load the readRDS function from R
     readRDS = robjects.r['readRDS']
@@ -25,5 +25,16 @@ def getRSigs():
     for i in range(len(r_object)):
         signatures_dict[r_object.names[i]] = r_object[i].tolist()
         singatures_groups_list.append(r_object.names[i])
+
+    mg = mygene.MyGeneInfo()
     
-    return signatures_dict, singatures_groups_list
+    symbols_dict_from_ensembl = {}
+    
+    for key in signatures_dict:
+        ensembl_ids = signatures_dict[key]
+        symbol_list = []
+        results = mg.querymany(ensembl_ids, scopes='ensembl.gene', fields='symbol', species='human')
+        symbols = [item.get('symbol', 'No Symbol') for item in results]  #will add 'No symbol' string if no symbol key is present
+        symbols_dict_from_ensembl[key] = symbols
+    
+    return symbols_dict_from_ensembl, singatures_groups_list
