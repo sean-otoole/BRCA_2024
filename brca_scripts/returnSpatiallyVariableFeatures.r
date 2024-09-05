acfb9e6b57a50b980e3d269d4ffbd92066df93af
Sean <sean.michael.otoole@gmail.com>
2024-09-05 14:35:05 +0000

    numerous pipeline additions
    added plotting and rank based spatial analysis, also R scripts to execute memory and cpu heavy tasks to operate outside of the notebook

---
+++ brca_scripts/returnSpatiallyVariableFeatures.r
@@ -0,0 +1,20 @@
+library(Seurat)
+library(future)
+setwd("/tmp/work/Visium/BRCA_2024")
+options(future.globals.maxSize = 15 * 1024^3)  # 10 GB
+plan("multicore", workers = 20)
+
+seurat_objects = readRDS('seurat_objects.rds')
+
+seurat_objects_list <- SplitObject(seurat_objects, split.by = "sample")  #split the samples
+
+seurat_objects_list <- lapply(seurat_objects_list, function(x) {
+  if ("SCT" %in% Assays(x)) {
+    FindSpatiallyVariableFeatures(x, assay = "SCT",  features = VariableFeatures(x)[1:3000],selection.method = "moransi")
+  } else {
+    warning("SCT assay not found in object")
+    return(x)
+  }
+})
+
+saveRDS(seurat_objects_list,'/tmp/work/Visium/BRCA_2024/seurat_objects_list.rds')
