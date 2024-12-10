08a0324920cdbd10651e87ea99863c798355ba8e
Sean <sean.michael.otoole@gmail.com>
2024-12-10 09:52:37 +0000

    added regress out parameter for mitochondrial percentage

--- BRCA_2024_pipeline/sctransform.r
+++ BRCA_2024_pipeline/sctransform.r
@@ -9,6 +9,6 @@ objects_path <- paste0(getwd(), '/objects/seurat_objects.rds')
 
 seurat_objects = readRDS(objects_path)
 seurat_objects <- SCTransform(seurat_objects, assay = "Spatial", verbose = TRUE, vst.flavor = "v2", conserve.memory=FALSE,
-                             return.only.var.genes = FALSE)
+                             return.only.var.genes = TRUE,  vars.to.regress = c('percent.mt'))  # originally return only variable genes was False
 saveRDS(seurat_objects, objects_path)
 cat("R script completed successfully\n")
